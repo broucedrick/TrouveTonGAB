d36a3903a30422c530f4e19a0e7d6e2774b879de
DIGISTALE FINANS <broucedrickatse@gmail.com>
2020-04-15 17:57:03 +0000

    connection ta php database

--- app/src/main/java/com/example/trouvetongab/ListAdapter.java
+++ app/src/main/java/com/example/trouvetongab/ListAdapter.java
@@ -13,11 +13,22 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListAdapter extends  RecyclerView.Adapter<ListAdapter.ViewHolder>{
     private ArrayList<String> dataset;
     private Context ctxt;
+
     public ListAdapter(Context context, ArrayList<String> data){
         dataset = data;
         ctxt = context;
@@ -52,8 +63,37 @@ public class ListAdapter extends  RecyclerView.Adapter<ListAdapter.ViewHolder>{
         holder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String URL_REGISTER = "http://10.0.2.2/androidapp/register.php";
                 Toast.makeText(ctxt, dataset.get(position), Toast.LENGTH_SHORT).show();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGISTER,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(ctxt, "Connection Success..."+response, Toast.LENGTH_SHORT).show();
+                                //ctxt.startActivity(new Intent(ctxt, ListGab.class));
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(ctxt, "Connection Error... "+error.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("user", "cedrick");
+                        params.put("user_name", "cedd");
+                        params.put("uset_pass", "1234");
+                        return params;
+                    }
+
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(ctxt);
+                requestQueue.add(stringRequest);
+
             }
+
         });
     }
 
